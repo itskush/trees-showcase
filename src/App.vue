c8f8b6d1b4099eab529359233278e94da55fdf76
An_kush <ankush.be.1401@gmail.com>
2023-01-07 23:39:27 +0200

    Ecosia commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template id="app">
+  <Search/>
+  <main class="body">
+    <Trees/>
+  </main>
+  <footer class="footer"></footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Trees from "./components/Trees.vue";
+import Search from "./components/Search.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Trees,
+    Search
+  },
+  created() {
+    document.title = "Ecosia Trees"
+    this.getList();
+  },
+  props: {
+  },
+  methods: {
+    getList() {
+      this.axios.get('https://s3.eu-central-1.amazonaws.com/ecosia-frontend-developer/trees.json').then((response) => {
+        console.log(response.data)
+      })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-color: #fff;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.header{
+  min-height: 100px;
+  background-color: #e6e7ec;
+  display: flex;
+  align-items: center;
+  .search {
+    min-width: 40%;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import mitt from 'mitt';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import './assets/tailwind.css'
+
+const emitter = mitt();
+const app = createApp(App)
+//event bus library
+app.config.globalProperties.emitter = emitter;
+app.use(VueAxios, axios)
+app.mount('#app')
